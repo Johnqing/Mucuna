7a4e6fbdcc94574b412d01a51f4939bba3f0f0b6
johnqing <csssnow@gmail.com>
2013-11-11 16:59:33 +0800

    examples js

---
+++ examples/js/combo/index.js
@@ -0,0 +1,5 @@
+(function() {
+	var srcPath = '../';
+	document.write('<script src="' + srcPath + 'component/slider.js"><\/script>');
+	document.write('<script src="' + srcPath + 'index/index.js"><\/script>');
+}());
---
+++ examples/js/component/slider.js
@@ -0,0 +1,7 @@
+(function(window){
+
+window.slider = function(){
+	
+}
+
+})(this);
